fc6e70a7d8e0b850723e331e541c3131fe3d7137
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-12-30 19:00:31 +0100

    Blacked graphe_1.py

--- graphe_1.py
+++ graphe_1.py
@@ -4,54 +4,54 @@
 # In[1]:
 
 
-#Comment gerer les erreurs ?
+# Comment gerer les erreurs ?
 class Cursor:
     __maxSizeX__ = 0
     __maxSizeY__ = 0
-    __currentPosition__ = [0,0]
-    
-    def __init__(self,sizeX,sizeY,index):
-        #erreur ?
-        x,y = index
+    __currentPosition__ = [0, 0]
+
+    def __init__(self, sizeX, sizeY, index):
+        # erreur ?
+        x, y = index
         self.__maxSizeX__ = sizeX
         self.__maxSizeY__ = sizeY
-        self.__currentPosition__ = [x,y]
-        
-    def getCurrentPosition(self):
-        return (self.__currentPosition__[0],self.__currentPosition__[1])
+        self.__currentPosition__ = [x, y]
 
-    def setCurrentPosition(self,x,y):
-        if x < 0 or x > self.__maxSizeX__ or y < 0 or y > self.__maxSizeY__ :
+    def getCurrentPosition(self):
+        return (self.__currentPosition__[0], self.__currentPosition__[1])
+
+    def setCurrentPosition(self, x, y):
+        if x < 0 or x > self.__maxSizeX__ or y < 0 or y > self.__maxSizeY__:
             print("Erreur setCurrentPosition")
             print("il faut gerer cette erreur")
         else:
             self.__currentPosition__[0] = x
             self.__currentPosition__[1] = y
-            
-    def cantMove(self,direction):
-        directionX,directionY = direction.value
-        
-        if(directionX == 0 and directionY == -1):
+
+    def cantMove(self, direction):
+        directionX, directionY = direction.value
+
+        if directionX == 0 and directionY == -1:
             return self.__currentPosition__[1] == 0
-        
-        if(directionX == -1 and directionY == 0):
+
+        if directionX == -1 and directionY == 0:
             return self.__currentPosition__[0] == 0
-        
-        if(directionX == 0 and directionY == 1):
+
+        if directionX == 0 and directionY == 1:
             return self.__currentPosition__[1] == self.__maxSizeY__ - 1
-        
-        if(directionX == 1 and directionY == 0):
+
+        if directionX == 1 and directionY == 0:
             return self.__currentPosition__[0] == self.__maxSizeX__ - 1
-        else :
+        else:
             print("il faut gerer cette erreur")
             return false
-        
-    def goTo(self,direction):
-        directionX,directionY = direction.value
+
+    def goTo(self, direction):
+        directionX, directionY = direction.value
         if self.cantMove(direction):
             print("Erreur goTo")
             print("il faut gerer cette erreur")
-        else :
+        else:
             self.__currentPosition__[0] = self.__currentPosition__[0] + directionX
             self.__currentPosition__[1] = self.__currentPosition__[1] + directionY
 
@@ -59,76 +59,79 @@ class Cursor:
 # In[12]:
 
 
-class Island():
+class Island:
     __bridgesRequired__ = 0
     __lNeighbors__ = []
     __maximumBridgeByDirection__ = 0
-    
-    def __init__(self,bridgesRequired,maximumBridgeByDirection):
+
+    def __init__(self, bridgesRequired, maximumBridgeByDirection):
         self.__bridgesRequired__ = bridgesRequired
-        self.__lNeighbors__ = [0,0,0,0]
+        self.__lNeighbors__ = [0, 0, 0, 0]
         self.__maximumBridgeByDirection__ = maximumBridgeByDirection
-    
-    def addPont(self,direction):
-        directionX,directionY = direction.value
+
+    def addPont(self, direction):
+        directionX, directionY = direction.value
         if self.__conditionADDBridge__(direction) == False:
             print("Le nombre maximum de pont avec cette ile est atteint")
             return
-        
-        if(directionX == 0 and directionY == -1):
+
+        if directionX == 0 and directionY == -1:
             self.__lNeighbors__[0] = self.__lNeighbors__[0] + 1
-        
-        elif(directionX == -1 and directionY == 0):
+
+        elif directionX == -1 and directionY == 0:
             self.__lNeighbors__[1] = self.__lNeighbors__[1] + 1
-        
-        elif(directionX == 0 and directionY == 1):
+
+        elif directionX == 0 and directionY == 1:
             self.__lNeighbors__[2] = self.__lNeighbors__[2] + 1
-        
-        elif(directionX == 1 and directionY == 0):
+
+        elif directionX == 1 and directionY == 0:
             self.__lNeighbors__[3] = self.__lNeighbors__[3] + 1
-        else :
+        else:
             print("il faut gerer cette erreur")
-        
-    def nbOfBridge(self,direction): 
-        directionX,directionY = direction.value
-        
-        if(directionX == 0 and directionY == -1):
+
+    def nbOfBridge(self, direction):
+        directionX, directionY = direction.value
+
+        if directionX == 0 and directionY == -1:
             return self.__lNeighbors__[0]
-        
-        elif(directionX == -1 and directionY == 0):
+
+        elif directionX == -1 and directionY == 0:
             return self.__lNeighbors__[1]
-        
-        elif(directionX == 0 and directionY == 1):
+
+        elif directionX == 0 and directionY == 1:
             return self.__lNeighbors__[2]
-        
-        elif(directionX == 1 and directionY == 0):
+
+        elif directionX == 1 and directionY == 0:
             return self.__lNeighbors__[3]
-        
-        else :
+
+        else:
             print("il faut gerer cette erreur")
             return self.__lNeighbors__
-    
-    def nbOfPossibleNewBridge(self,direction):
-        return min(self.bridgesRequired() - self.someOfBridge(),
-                    self.maximumBridgeByDirection() - self.nbOfBridge(direction))
-    
+
+    def nbOfPossibleNewBridge(self, direction):
+        return min(
+            self.bridgesRequired() - self.someOfBridge(),
+            self.maximumBridgeByDirection() - self.nbOfBridge(direction),
+        )
+
     def someOfBridge(self):
         return sum(self.__lNeighbors__)
-    
+
     def maximumBridgeByDirection(self):
         return self.__maximumBridgeByDirection__
-    
+
     def bridgesRequired(self):
         return self.__bridgesRequired__
-        
-    def __conditionADDBridge__(self,direction):
+
+    def __conditionADDBridge__(self, direction):
         if self.nbOfBridge(direction) >= self.__maximumBridgeByDirection__:
             return False
         if sum(self.__lNeighbors__) >= self.__bridgesRequired__:
             return False
         return True
 
-i = Island(4,2)
+
+i = Island(4, 2)
 i.addPont(Direction.LEFT)
 i.addPont(Direction.RIGHT)
 i.addPont(Direction.UP)
@@ -143,13 +146,13 @@ print(i.nbOfPossibleNewBridge(Direction.RIGHT))
 
 from enum import Enum
 
-#un Enum est-il vraiment interessant ? 
+# un Enum est-il vraiment interessant ?
 class Direction(Enum):
-    LEFT = (0,-1)
-    UP = (-1,0)
-    RIGHT = (0,1)
-    DOWN = (1,0)
-    
+    LEFT = (0, -1)
+    UP = (-1, 0)
+    RIGHT = (0, 1)
+    DOWN = (1, 0)
+
     def opposite(direction):
         if direction == Direction.LEFT:
             return Direction.RIGHT
@@ -160,73 +163,87 @@ class Direction(Enum):
         if direction == Direction.DOWN:
             return Direction.UP
         print("erreur")
+
     opposite = staticmethod(opposite)
-            
+
+
 Direction.opposite(Direction.DOWN)
 
 
 # In[ ]:
 
 
-
-
-
 # In[171]:
 
 
-class Hashiwokakera():
-    
-    def __init__(self,graphe):
+class Hashiwokakera:
+    def __init__(self, graphe):
         ##tester le graphe
         self.graphe = graphe
-        x,y = graphe.shape
-        self.cursor = Cursor(x,y,(0,0))
+        x, y = graphe.shape
+        self.cursor = Cursor(x, y, (0, 0))
         self.__allPosibilityOfBridge__ = createPossibility()
-    
+
     def __createBridge__(self):
         possibleBridge = self.__allPosibilityOfBridge__[:]
-        
+
         nbOfLeftPossibleBridge = self.__foundNbOfPossibleBridge__(Direction.LEFT)
         nbOfUpPossibleBridge = self.__foundNbOfPossibleBridge__(Direction.UP)
         nbOfRightPossibleBridge = self.__foundNbOfPossibleBridge__(Direction.RIGHT)
         nbOfDownPossibleBridge = self.__foundNbOfPossibleBridge__(Direction.DOWN)
-        
+
         print(nbOfLeftPossibleBridge)
         print(nbOfUpPossibleBridge)
         print(nbOfRightPossibleBridge)
         print(nbOfDownPossibleBridge)
-        
-        possibleBridge = [i for i in possibleBridge if i[0] < nbOfLeftPossibleBridge + 1]
+
+        possibleBridge = [
+            i for i in possibleBridge if i[0] < nbOfLeftPossibleBridge + 1
+        ]
         possibleBridge = [i for i in possibleBridge if i[1] < nbOfUpPossibleBridge + 1]
-        possibleBridge = [i for i in possibleBridge if i[2] < nbOfRightPossibleBridge + 1]
-        possibleBridge = [i for i in possibleBridge if i[3] < nbOfDownPossibleBridge + 1]
-        possibleBridge = [i for i in possibleBridge 
-                          if sum(i) == self.graphe[self.cursor.getCurrentPosition()].bridgesRequired()
-                             - self.graphe[self.cursor.getCurrentPosition()].someOfBridge()]
+        possibleBridge = [
+            i for i in possibleBridge if i[2] < nbOfRightPossibleBridge + 1
+        ]
+        possibleBridge = [
+            i for i in possibleBridge if i[3] < nbOfDownPossibleBridge + 1
+        ]
+        possibleBridge = [
+            i
+            for i in possibleBridge
+            if sum(i)
+            == self.graphe[self.cursor.getCurrentPosition()].bridgesRequired()
+            - self.graphe[self.cursor.getCurrentPosition()].someOfBridge()
+        ]
         return possibleBridge
-        
-    def __foundNbOfPossibleBridge__(self,direction):
-        #Comment gerer un mauvais index ?
-        directionX,directionY = direction.value
-        #erreur ?
-        a,b = self.graphe.shape
-        cursor = Cursor(a,b,self.cursor.getCurrentPosition())
+
+    def __foundNbOfPossibleBridge__(self, direction):
+        # Comment gerer un mauvais index ?
+        directionX, directionY = direction.value
+        # erreur ?
+        a, b = self.graphe.shape
+        cursor = Cursor(a, b, self.cursor.getCurrentPosition())
         cursor.goTo(direction)
-        while(type(self.graphe[cursor.getCurrentPosition()]) != Island 
-              and (self.graphe[cursor.getCurrentPosition()] != -1
-                   or self.graphe[self.cursor.getCurrentPosition()].nbOfBridge(direction) == 1)
-                       and not cursor.cantMove(direction)):
+        while (
+            type(self.graphe[cursor.getCurrentPosition()]) != Island
+            and (
+                self.graphe[cursor.getCurrentPosition()] != -1
+                or self.graphe[self.cursor.getCurrentPosition()].nbOfBridge(direction)
+                == 1
+            )
+            and not cursor.cantMove(direction)
+        ):
             print("coucou")
             cursor.goTo(direction)
-            
+
         if self.cursor.getCurrentPosition() == cursor.getCurrentPosition():
             return 0
         if self.graphe[cursor.getCurrentPosition()] == -1:
             return 0
         if self.graphe[cursor.getCurrentPosition()] == 0:
             return 0
-        return self.graphe[cursor.getCurrentPosition()].nbOfPossibleNewBridge(Direction.opposite(direction))
-    
+        return self.graphe[cursor.getCurrentPosition()].nbOfPossibleNewBridge(
+            Direction.opposite(direction)
+        )
 
 
 # In[170]:
@@ -234,20 +251,25 @@ class Hashiwokakera():
 
 import numpy as np
 
-graphe = np.array([[Island(4,2),-1,Island(4,2)],
-                   [0,Island(0,0),Island(0,0)],
-                   [Island(1,2),Island(0,0),Island(0,0)]], dtype = object)
+graphe = np.array(
+    [
+        [Island(4, 2), -1, Island(4, 2)],
+        [0, Island(0, 0), Island(0, 0)],
+        [Island(1, 2), Island(0, 0), Island(0, 0)],
+    ],
+    dtype=object,
+)
 
-i = Island(4,2)
+i = Island(4, 2)
 i.addPont(Direction.LEFT)
-graphe[1,2] = i
+graphe[1, 2] = i
 
-i = Island(4,2)
+i = Island(4, 2)
 i.addPont(Direction.RIGHT)
-graphe[0,0] = i
+graphe[0, 0] = i
 
 hashiwokakera = Hashiwokakera(graphe)
-hashiwokakera.cursor.setCurrentPosition(0,0)
+hashiwokakera.cursor.setCurrentPosition(0, 0)
 hashiwokakera.__createBridge__()
 
 
@@ -288,11 +310,11 @@ S
 # In[10]:
 
 
-#la rendre plus flexible
+# la rendre plus flexible
 def createPossibility():
     l = []
     for i0 in range(3):
-        buf = [0,0,0,0]
+        buf = [0, 0, 0, 0]
         buf[0] = i0
         for i1 in range(3):
             buf[1] = i1
@@ -303,8 +325,9 @@ def createPossibility():
                     l.append(buf[:])
     return l
 
-#renvoie une listes dont les éléments sont les nombres d'occurances des éléemnts l1 dans l0
-def compteOccurence(l0,l1):
+
+# renvoie une listes dont les éléments sont les nombres d'occurances des éléemnts l1 dans l0
+def compteOccurence(l0, l1):
     s = 0
     r = [0 for i in range(len(l1))]
     for i in range(len(l1)):
@@ -312,6 +335,7 @@ def compteOccurence(l0,l1):
             if l1[i] == j:
                 r[i] = r[i] + 1
     return r
+
 
 l = createPossibility()
 l = [i for i in l if i[0] < 1]
@@ -325,41 +349,19 @@ print(l)
 # In[ ]:
 
 
-
+# In[ ]:
 
 
 # In[ ]:
 
 
-
+# In[ ]:
 
 
 # In[ ]:
 
 
-
-
-
 # In[ ]:
 
 
-
-
-
 # In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
