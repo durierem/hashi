476b3e5a32ca8c03d75101d945d803bdcfbb9073
Rémi Durieu <remi.durieu@univ-rouen.fr>
2020-12-30 17:06:03 +0100

    Fix main.py

--- main.py
+++ main.py
@@ -1,20 +1,21 @@
 from cell import Cell
 from cell import CellType
 from direction import Direction
+from grid import Grid
 from island import Island
 
 def main():
     matrix = [
         [Cell(), Cell(), Cell(Island(2))],
-        [Cell(Island(1)), Cell(), Cell(Island(4))],
-        [Cell(), Cell(), Cell(Island(1))]
+        [Cell(Island(2)), Cell(), Cell(Island(4))],
+        [Cell(), Cell(), Cell(Island(1))],
+        [Cell(Island(1)), Cell(), Cell()]
     ]
 
     grid = Grid(matrix)
-    print(grid.getWidth())
-    print(grid.getHeight())
+    print("Grid width: " + str(grid.getWidth()))
+    print("Grid height: " + str(grid.getHeight()))
 
-    for line in grid:
-        for cell in line:
-            print(str(cell.getType()))
+    # grid.display() OU print(grid) + redéfinition de Grid.__str__
+
 main()
