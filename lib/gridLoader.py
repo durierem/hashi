bc47d20125014a3b21b069fc93c3ef2f48bd8d9a
MrAlienTech <kaci.hmd76@gmail.com>
2021-01-10 18:16:15 +0100

    Ajout de vérification avant chargement d'une grille

--- lib/gridLoader.py
+++ lib/gridLoader.py
@@ -4,6 +4,11 @@ from lib.cell import Cell
 from lib.grid import Grid
 from lib.island import Island
 
+class IllegalValue(Exception):
+    pass
+
+class BadDataFormat(Exception):
+    pass
 
 class GridLoader:
     @staticmethod
@@ -12,13 +17,20 @@ class GridLoader:
             with open(file) as f:
                 data = json.load(f)
             matrix = []
+            length = -1
             for arr in data["grid"]:
                 column = []
                 for x in arr:
+                    if x < 0 or x > 8 :
+                        raise IllegalValue("Illegal value in the json grid")
                     if x == 0:
                         column.append(Cell())
                     if x > 0:
                         column.append(Cell(Island(x)))
+                if length == -1:
+                    length = len(column)
+                if not len(column) == length:
+                    raise BadDataFormat("One of the line in json grid is shorter")
                 matrix.append(column)
             return Grid(matrix)
         except Exception as e:
